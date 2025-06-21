132b595eb89d123a8bedd566bb13f84612853ee5
David Runemalm <david.runemalm@gmail.com>
2025-06-22 00:55:53 +0200

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='codius',
-    version='0.1.0-alpha.2',
+    version='0.1.0-alpha.3',
     package_dir={'': 'src'},
     packages=find_packages(where='src'),
     include_package_data=True,
